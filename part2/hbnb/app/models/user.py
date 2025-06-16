d84599009737c57675d36941bcd4ca5f3f245efc
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-16 14:16:33 +0200

    define classes for user, amenity, place and reviews

---
+++ part2/hbnb/app/models/user.py
@@ -0,0 +1,40 @@
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """Update the updated_at timestamp whenever the object is modified"""
+        self.updated_at = datetime.now()
+
+    def update(self, data):
+        """Update the attributes of the object based on the provided dictionary"""
+        for key, value in data.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
+        self.save()  # Update the updated_at timestamp
+
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, is_admin):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.is_admin = is_admin
+        self.places = []
+
+    def owns_place(self, place):
+        self.places.append(place)
+
+    def register_user(self):
+        pass
+
+    def update_user(self):
+        pass
+
+    def delete_user(self):
+        pass
