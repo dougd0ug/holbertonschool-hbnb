fe17a16fd19269e898cf7bc2279873731288410c
dougd0ug <dorine.sgv@hotmail.fr>
2025-06-17 11:51:49 +0200

    fix: the conditions for def init, and verify the format for each value

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -5,12 +5,12 @@ from user import BaseModel
 class Amenity(BaseModel):
     def __init__(self, name):
         super().__init__()
-        self.name = name
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError("Name must be a string.")
 
     def create_amenity(self):
-        pass
-
-    def update_amenity(self):
         pass
 
     def delete_amenity(self):
--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -1,5 +1,7 @@
 import uuid
 from datetime import datetime
+import re
+
 
 class BaseModel:
     def __init__(self):
@@ -19,21 +21,39 @@ class BaseModel:
         self.save()  # Update the updated_at timestamp
 
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email, is_admin):
+    def __init__(self, first_name, last_name, email, is_admin=False):
         super().__init__()
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.is_admin = is_admin
+
+        if len(first_name) <= 50 and isinstance(first_name, str):
+            self.first_name = first_name
+        else:
+            raise ValueError("Your first name must be a string and have 50 characters maximum.")
+
+        if len(last_name) <= 50 and isinstance(last_name, str):
+            self.last_name = last_name
+        else:
+            raise ValueError("Your last name must be a string and have 50 characters maximum.")
+
+        if self.valid_email(email):
+            self.email = email
+        else:
+            raise ValueError("Mail adress is not valid.")
+
+        if isinstance(is_admin, bool):
+            self.is_admin = is_admin
+        else:
+            raise TypeError("You must be either a user or an admin.")
         self.places = []
+
+    @staticmethod
+    def valid_email(email):
+        pattern = r"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$"
+        return re.match(pattern, email) is not None
 
     def owns_place(self, place):
         self.places.append(place)
 
     def register_user(self):
-        pass
-
-    def update_user(self):
         pass
 
     def delete_user(self):
