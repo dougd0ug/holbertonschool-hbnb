f2de849a0e2072858e273d5e51787e6cf35a4bac
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-17 11:57:22 +0200

    refactor: validate input parameters in Amenity, Place, Review, and User models

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -1,19 +1,45 @@
 import uuid
 from datetime import datetime
-from user import BaseModel
+from app.models.user import BaseModel, User
+from app.persistence.repository import InMemoryRepository
+
 
 class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner):
         super().__init__()
-        self.title = title
-        self.description = description
-        self.price = price
-        self.latitude = latitude
-        self.longitude = longitude
+
+        if len(title) <= 100 and isinstance(title, str):
+            self.title = title
+        else:
+            raise ValueError("Title must be a string with 100 characters maximum.")
+
+        if isinstance(description, str):
+            self.description = description
+        else:
+            raise TypeError("Description must be a string.")
+
+        if price > 0 and isinstance(price, float):
+            self.price = price
+        else:
+            raise ValueError("Price must be over 0.")
+
+        if isinstance(latitude, float) and latitude >= -90.0 and latitude <= 90.0:
+            self.latitude = latitude
+        else:
+            raise ValueError("Latitude must be between -90.0 and 90.0.")
+
+        if isinstance(longitude, float) and longitude >= -180.0 and longitude <= 180.0:
+            self.longitude = longitude
+        else:
+            raise ValueError("Latitude must be between -180.0 and 180.0.")
+
+        owner = user_repository.get(owner_id)
+        if not isinstance(owner, User):
+            raise ValueError("User doesn't exist.")
         self.owner = owner
         self.reviews = []
         self.amenities = []
-    
+
     def add_review(self, review):
         """Add a review to the place."""
         self.reviews.append(review)
@@ -23,9 +49,6 @@ class Place(BaseModel):
         self.amenities.append(amenity)
 
     def create_place(self):
-        pass
-
-    def update_place(self):
         pass
 
     def delete_place(self):
--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -1,19 +1,32 @@
 import uuid
 from datetime import datetime
-from user import BaseModel
+from app.models.user import BaseModel
+from app.models.place import Place
+from app.models.user import User
 
 class Review(BaseModel):
     def __init__(self, text, rating, place, user):
         super().__init__()
-        self.text = text
-        self.rating = rating
+        if isinstance(text, str):
+            self.text = text
+        else:
+            raise TypeError("The content of the review must be a string.")
+
+        if isinstance(rating, int) and rating >= 1 and rating >= 5:
+            self.rating = rating
+        else:
+            raise ValueError("The rating must be between 1 and 5")
+
+        place = place_repository.get(place_id)
+        if not isinstance(place, Place):
+            raise ValueError("Place doesn't exist.")
         self.place = place
+        user = user_repository.get(user_id)
+        if not isinstance(user, User):
+            raise ValueError("User doesn't exist.")
         self.user = user
 
     def create_review(self):
-        pass
-
-    def update_review(self):
         pass
 
     def delete_review(self):
