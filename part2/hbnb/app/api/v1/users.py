e27c09557816538e690513d38a6dfd63e7c09a56
Ancelin-31 <ancelin.chevallier@gmail.com>
2025-06-18 16:05:04 +0200

    add get all user method in UserList

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -41,6 +41,17 @@ class UserList(Resource):
         
         updated_user = facade.put_user
 
+    def get_by_email(self, user_email):
+        """Get user details by email"""
+        user = facade.get_user_by_email(user_email)
+        if not user:
+            return {'error': 'User not found'}, 404
+        return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+
+    def get(self):
+        users = self.user_repo.get_all()
+        return [user.to_dict() for user in users], 200
+
 @api.route('/<user_id>')
 class UserResource(Resource):
     @api.response(200, 'User details retrieved successfully')
@@ -51,11 +62,3 @@ class UserResource(Resource):
         if not user:
             return {'error': 'User not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
-
-    def get_by_email(self, user_email):
-        """Get user details by email"""
-        user = facade.get_user_by_email(user_email)
-        if not user:
-            return {'error': 'User not found'}, 404
-        return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
-    
