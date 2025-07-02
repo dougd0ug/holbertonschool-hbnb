65112befd11d0ec2dfa3df53aa69a4fefc2fd1cf
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-02 10:19:53 +0200

    add auth file with JWT initialisation

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_restx import Api
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 from app.api.v1.users import api as users_ns
 from app.api.v1.places import api as place_ns
 from app.api.v1.amenities import api as amenity_ns
 from app.api.v1.reviews import api as review_ns
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
@@ -21,5 +23,6 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(review_ns, path='/api/v1/reviews')
 
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     return app
