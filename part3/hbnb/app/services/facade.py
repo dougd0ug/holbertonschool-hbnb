a2cd3751eaeae7c1d1b27f825a60a30f02fc6cbc
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-10 15:40:30 +0200

    add new facade and user repository

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -1,5 +1,6 @@
 from app.persistence.repository import InMemoryRepository
 from app.persistence.repository import SQLAlchemyRepository
+from app.services.repositories.user_repository import UserRepository
 from app.models.user import User, BaseModel
 from app.models.amenity import Amenity
 from app.models.review import Review
@@ -8,18 +9,19 @@ from app.models.place import Place
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = SQLAlchemyRepository(User)
+        self.user_repo = UserRepository()
 
     def create_user(self, user_data):
         user = User(**user_data)
+        user.hash_password(user_data['password'])
         self.user_repo.add(user)
         return user
 
     def get_user(self, user_id):
         return self.user_repo.get(user_id)
-    
+
     def get_user_by_email(self, email):
-        return self.user_repo.get_by_attribute('email', email)
+        return self.user_repo.get_user_by_email(email)
     
     def get_all_users(self):
         return self.user_repo.get_all()
