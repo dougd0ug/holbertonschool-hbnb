d7c72ad37da2311d244a37c8e81313619b20e042
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-10 11:08:27 +0200

    update facade for SQLAlchelmy

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -1,4 +1,5 @@
 from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import SQLAlchemyRepository
 from app.models.user import User, BaseModel
 from app.models.amenity import Amenity
 from app.models.review import Review
@@ -7,17 +8,13 @@ from app.models.place import Place
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)
 
-    # Placeholder method for creating a user
     def create_user(self, user_data):
         user = User(**user_data)
         self.user_repo.add(user)
         return user
-    
+
     def get_user(self, user_id):
         return self.user_repo.get(user_id)
     
