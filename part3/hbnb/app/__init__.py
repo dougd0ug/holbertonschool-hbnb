833b9ca67fbf348ff20d4c5b8c166ae8fa73dbc3
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-11 20:29:29 +0200

    change init for auth

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -15,11 +15,27 @@ from app.api.v1.reviews import api as review_ns
 from app.api.v1.auth import api as auth_ns
 from app.api.v1.admin import api as admin_ns
 
+authorizations = {
+    'Bearer': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'Authorization',
+        'description': 'JWT avec le schéma Bearer. Ex : **Bearer &lt;votre_token&gt;**'
+    }
+}
+
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+    api = Api(
+        app,
+        version='1.0',
+        title='HBnB API',
+        description='HBnB Application API',
+        doc='/docs',  # Swagger accessible ici
+        authorizations=authorizations,
+    )
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
