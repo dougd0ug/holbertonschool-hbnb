05de70344f7265974e9822887290234bbd29eb94
dougd0ug <dorine.sgv@hotmail.fr>
2025-07-24 16:06:09 +0200

    implement login with db

---
+++ part4/scripts.js
@@ -0,0 +1,46 @@
+/* 
+  This is a SAMPLE FILE to get you started.
+  Please, follow the project instructions to complete the tasks.
+*/
+
+document.addEventListener('DOMContentLoaded', () => {
+    /* DO SOMETHING */
+  });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', async (event) => {
+            event.preventDefault();
+            // Your code to handle form submission
+            const email = document.getElementById('email').value;
+            const password = document.getElementById('password').value;
+            try {
+                const response = await loginUser(email, password);
+                console.log("ok");
+                if (response.ok) {
+                    const data = await response.json();
+                    document.cookie = `token=${data.access_token}; path=/`;
+                    window.location.href = 'index.html';
+                } else {
+                    alert('Login failed: ' + response.statusText);
+                }
+                } catch (error) {
+                    alert("Connexion error.");
+                }
+        });
+    }
+});
+
+async function loginUser(email, password) {
+    const response = await fetch('http://localhost:5000/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+    });
+    // Handle the response
+    return response;
+}
